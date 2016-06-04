76962adef8d9006ff688c06ffa886c3b9cbf40e7
ykim306 <ykim306@gmail.com>
2016-06-04 16:27:47 -0400

    clean up - butterknife liabray and standard method of display text in toast

--- app/src/main/java/app/com/example/android/myappportfolio/MainActivity.java
+++ app/src/main/java/app/com/example/android/myappportfolio/MainActivity.java
@@ -1,14 +1,9 @@
 package app.com.example.android.myappportfolio;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
-
-/*import butterknife.ButterKnife;
-import butterknife.OnClick;*/
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,8 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // below for ButterKnife library http://jakewharton.github.io/butterknife/
-        // ButterKnife.inject(this);
     }
 
     /** Called when the user touches the project01 button */
@@ -51,26 +44,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void DisplayText(String toastText){
-
-        // standard way
-        /*Context context = getApplicationContext();
-        CharSequence text = "This button will launch my Popular Movies app!";
-        int duration = Toast.LENGTH_SHORT;
-
-        Toast.makeText(context, toastText, duration).show();*/
-
         Toast.makeText(MainActivity.this, toastText, Toast.LENGTH_SHORT).show();
     }
-
-    // below for ButterKnife library http://jakewharton.github.io/butterknife/
-    /*@OnClick({ R.id.button,
-            R.id.button2,
-            R.id.button3,
-            R.id.button4,
-            R.id.button5,
-            R.id.button6})
-    public void showToast(Button button) {
-        Toast.makeText(this, String.format(getString(R.string.button_message), button.getText()), Toast.LENGTH_SHORT).show();
-    }*/
 
 }
